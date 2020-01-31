c138272feb4481e48090b1ac75a516a6a0108b94
Annie Huang <annie.hn@gmail.com>
2020-02-01 10:25:00 +1100

    First typescript. Add two numbers

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Your code goes here....');
+function add(n1, n2) {
+    console.log(typeof number1);
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    //     throw new Error('Incorrect input!');
+    // }
+    return n1 + n2;
+}
+// const number1 = '5';
+var number1 = 5; // this is the same as 5.0. Javascript does not have the concept of int and float
+var number2 = 2.8;
+var result = add(number1, number2);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,14 @@
-console.log('Your code goes here....');
+function add(n1: number, n2: number) {
+    console.log(typeof number1);
+    // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+    //     throw new Error('Incorrect input!');
+    // }
+    return n1 + n2;
+}
+
+// const number1 = '5';
+const number1 = 5; // this is the same as 5.0. Javascript does not have the concept of int and float
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(result);
