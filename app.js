173196eed9cac8016ea43f932a44b15587ee8337
Annie Huang <annie.hn@gmail.com>
2020-02-01 16:21:39 +1100

    Explain why DOM is available in the current es6 settings in tsconfig.json

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 "use strict";
-let age;
-age = 30;
-const userName = 'Maximilian';
-console.log(userName);
+// let age;
+// age = 30;
+// const userName = 'Maximilian';
+// console.log(userName);
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    console.log('Clicked!');
+});
+// because we set
+// "target": "es6",
+// and comment out // "lib": [],
+// by default it includes everything is available in es6
+// plus all DOM element, so you can just access document, querySelector etc.
+// Some of the es6 features include:
+const map = new Map();
