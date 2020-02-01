75650fab0baf05259da9875e4dd73f20199a95a0
Annie Huang <annie.hn@gmail.com>
2020-02-01 15:01:47 +1100

    never in typescript

--- app.js
+++ app.js
@@ -7,3 +7,8 @@ userInput = 'Max';
 if (typeof userInput === 'string') {
     userName = userInput;
 }
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+var result = generateError('An error occurred!', 500);
+console.log(result);
--- app.ts
+++ app.ts
@@ -11,4 +11,11 @@ if (typeof userInput === 'string') {
 }
 
 
+function generateError(message: string, code: number): never {
+    throw {message: message, errorCode: code};
+    // while(true) {}
+}
+
+const result = generateError('An error occurred!', 500);
+console.log(result);
 
