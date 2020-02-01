a18f3172fbfb198405bf2a110f4a71de72ea6fac
Annie Huang <annie.hn@gmail.com>
2020-02-01 14:57:40 +1100

    unknown in typescript

---
+++ app.js
@@ -0,0 +1,9 @@
+var userInput;
+// let userInput: any;
+var userName;
+userInput = 5;
+userInput = 'Max';
+// userName = userInput;
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
---
+++ app.ts
@@ -0,0 +1,14 @@
+let userInput: unknown;
+// let userInput: any;
+let userName: string;
+
+userInput = 5;
+userInput = 'Max';
+
+// userName = userInput;
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+
+
+
