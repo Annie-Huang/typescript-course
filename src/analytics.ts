839d349c6969235627c6a5d937a1b26b5df47f4a
Annie Huang <annie.hn@gmail.com>
2020-02-01 17:35:36 +1100

    Keep "noImplicitAny": true, so function parameter has to assign to a type, otherwise, compile error.

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,1 +1,14 @@
-console.log('Sending...');
+// console.log('Sending...');
+
+// we allow variable to not assigned to a type when it is declared.
+let logged;
+
+// We will not allow parameter to be not assigned to a type when it's declared:
+// function sendAnalytics(data) {
+function sendAnalytics(data: string) {
+    console.log(data);
+    logged = true;
+    console.log(logged);
+}
+
+sendAnalytics('The data');
