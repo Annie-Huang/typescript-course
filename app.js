9b24359f07fc1e6a028ede2be45bae7c44d8c4ba
Annie Huang <annie.hn@gmail.com>
2020-02-01 11:25:56 +1100

    Try the boolean and string type

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
-function add(n1, n2) {
-    console.log(typeof number1);
+function add(n1, n2, showResult, phrase) {
+    // console.log(typeof number1);
     // if (typeof n1 !== 'number' || typeof n2 !== 'number') {
     //     throw new Error('Incorrect input!');
     // }
-    return n1 + n2;
+    var result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
 }
 // const number1 = '5';
 var number1 = 5; // this is the same as 5.0. Javascript does not have the concept of int and float
 var number2 = 2.8;
-var result = add(number1, number2);
-console.log(result);
+var printResult = true;
+var resultPhrase = 'Result is: ';
+// const result = add(number1, number2, printResult);
+add(number1, number2, printResult, resultPhrase);
+// console.log(result);
